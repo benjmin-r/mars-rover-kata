e2e5d85c8a54f51bbbe230e0a9093c0fa29d7ca6
Benjamin Reitzammer <benjamin@ambestengestern.de>
2013-04-11 18:11:33 +0200

    implemented all directions and movements

--- marsrover.py
+++ marsrover.py
@@ -24,6 +24,12 @@ class Position():
     def up(self):
         return Position(self.xpos+1, self.ypos)
 
+    def left(self):
+        return Position(self.xpos, self.ypos-1)
+
+    def right(self):
+        return Position(self.xpos, self.ypos+1)
+
     def get_position(self):
         return (self.xpos, self.ypos)
 
@@ -31,8 +37,38 @@ class Direction():
 
     @staticmethod
     def for_direction(direction_str):
+        if direction_str == "west":
+            return WestDirection()
+        if direction_str == "east":
+            return EastDirection()
+        if direction_str == "north":
+            return NorthDirection()
         if direction_str == "south":
             return SouthDirection()
+
+class EastDirection():
+
+    def move(self, position, command):
+        if command == 'f':
+            return position.right()
+        if command == 'b':
+            return position.left()
+
+class NorthDirection():
+
+    def move(self, position, command):
+        if command == 'f':
+            return position.up()
+        if command == 'b':
+            return position.down()
+
+class WestDirection():
+
+    def move(self, position, command):
+        if command == 'f':
+            return position.left()
+        if command == 'b':
+            return position.right()
 
 class SouthDirection():
 
--- tests.py
+++ tests.py
@@ -20,5 +20,20 @@ class RoverTest(unittest.TestCase):
         rover.move(('f', 'f', 'b'))
         self.assertEquals((-1, 0), rover.get_position())
 
+    def test_move_one_forward_three_backward_west(self):
+        rover = Rover(Position(0, 0), Direction.for_direction("west"))
+        rover.move(('f', 'b', 'b', 'b'))
+        self.assertEquals((0, 2), rover.get_position())
+
+    def test_move_two_forward_one_backwards_north(self):
+        rover = Rover(Position(0, 0), Direction.for_direction("north"))
+        rover.move(('f', 'f', 'b'))
+        self.assertEquals((1, 0), rover.get_position())
+
+    def test_move_five_forward_eight_backwards_east(self):
+        rover = Rover(Position(0, 0), Direction.for_direction("east"))
+        rover.move(('f', 'f', 'f', 'f', 'f', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b',))
+        self.assertEquals((0, -3), rover.get_position())
+
 if __name__ == '__main__':
     unittest.main()
