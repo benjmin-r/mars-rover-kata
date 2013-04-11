e45e3501b0d30e6732cb64810beb47f20bb44e40
Benjamin Reitzammer <benjamin@ambestengestern.de>
2013-04-11 15:40:26 +0200

    refactored positions into own class

--- marsrover.py
+++ marsrover.py
@@ -1,17 +1,31 @@
 
 class Rover():
 
-    def __init__(self, xpos, ypos, direction):
-        self.xpos = xpos
-        self.ypos = ypos
+    def __init__(self, position, direction):
+        self.position = position
         self.direction = direction
 
     def move(self, commands=()):
         for cmd in commands:
             if cmd == 'f' and self.direction == 'south':
-                self.xpos -= 1
+                self.position = self.position.down()
             if cmd == 'b' and self.direction == 'south':
-                self.xpos += 1
+                self.position = self.position.up()
+
+    def get_position(self):
+        return self.position.get_position()
+
+class Position():
+
+    def __init__(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+
+    def down(self):
+        return Position(self.xpos-1, self.ypos)
+
+    def up(self):
+        return Position(self.xpos+1, self.ypos)
 
     def get_position(self):
         return (self.xpos, self.ypos)
--- tests.py
+++ tests.py
@@ -1,22 +1,22 @@
 import unittest
 
-from marsrover import Rover
+from marsrover import Rover, Position
 
 
 class RoverTest(unittest.TestCase):
 
     def test_move_one_forward(self):
-        rover = Rover(0, 0, "south")
+        rover = Rover(Position(0, 0), "south")
         rover.move(('f',))
         self.assertEquals((-1, 0), rover.get_position())
 
     def test_move_three_forward(self):
-        rover = Rover(0, 0, "south")
+        rover = Rover(Position(0, 0), "south")
         rover.move(('f', 'f', 'f'))
         self.assertEquals((-3, 0), rover.get_position())
 
     def test_move_two_forward_one_backwards(self):
-        rover = Rover(0, 0, "south")
+        rover = Rover(Position(0, 0), "south")
         rover.move(('f', 'f', 'b'))
         self.assertEquals((-1, 0), rover.get_position())
 
