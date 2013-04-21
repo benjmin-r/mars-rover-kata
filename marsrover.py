1da70e2f597d8b20b798d41d1ce55ab9f9bc507f
Benjamin Reitzammer <benjamin@ambestengestern.de>
2013-04-21 23:14:16 +0200

    refactored south movement into own direction class

--- marsrover.py
+++ marsrover.py
@@ -3,13 +3,18 @@ class Rover():
 
     def __init__(self, position, direction):
         self.position = position
+        self.direction = direction
 
     def move(self, commands):
         for cmd in commands:
-            if cmd == 'f':
-                self.position.down()
-            else:
+            if cmd == 'f' and self.direction == Direction.S:
+                self.direction.forward(self.position)
+            elif cmd == 'b' and self.direction == Direction.S:
+                self.direction.backward(self.position)
+            elif cmd == 'f' and self.direction == Direction.N:
                 self.position.up()
+            elif cmd == 'b' and self.direction == Direction.N:
+                self.position.down()
 
     def get_position(self):
         return self.position
@@ -41,5 +46,15 @@ class Position():
     def up(self):
         self.pos_x += 1
 
+class SouthDirection():
+
+    def forward(self, position):
+        return position.down()
+
+    def backward(self, position):
+        return position.up()
+
 class Direction():
-    S = 'south'
+    S = SouthDirection()
+    N = 'north'
+
